a17a2a5d17fe5456bc73edfe58db7451fc50b8ef
branrojs <branrojs@gmail.com>
2018-04-27 21:46:14 +0000

    adding stock and users relations, and stock saving for users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  resources :user_stocks, only:[:create]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
